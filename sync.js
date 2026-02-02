4c0fc7a267fb29713ab243bfbb3950931b8ffa09
wira-bfi <komang.apriyana@bfi.co.id>
2026-02-02 10:29:51 +0700

    sync

--- sync.js
+++ sync.js
@@ -169,7 +169,7 @@ async function main() {
           allReadSets.push(...readSets);
         }
 
-        if (fileName === 'dp-ndf-v0_9_0.go') {
+        if (fileName === 'dp-ndf-v0_10_0.go') {
           const constants = extractConstants(content);
           Object.assign(allConstants, constants);
         }
